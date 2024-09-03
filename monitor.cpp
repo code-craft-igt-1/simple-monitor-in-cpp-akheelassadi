c26e79dc2557d0a8a08015f11099c6d05d5eda95
Mohammed Akheel Assadi <MohammedAkheel.Assadi@philips.com>
2024-09-03 16:04:35 +0530

    initial commit

--- monitor.cpp
+++ monitor.cpp
@@ -5,33 +5,39 @@
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
+bool isTemperatureCritical(float temperature) {
+  return (temperature > 102 || temperature < 95);
+}
+
+bool isPulseRateCritical(float pulseRate) {
+  return (pulseRate < 60 || pulseRate > 100);
+}
+
+bool isSpo2Critical(float spo2) {
+  return (spo2 < 90);
+}
+
+void displayWarning(const std::string& message) {
+  cout << message << "\n";
+  for (int i = 0; i < 6; i++) {
+    cout << "\r* " << flush;
+    sleep_for(seconds(1));
+    cout << "\r *" << flush;
+    sleep_for(seconds(1));
+  }
+}
+
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+  if (isTemperatureCritical(temperature)) {
+    displayWarning("Temperature is critical!");
     return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+  } 
+  if (isPulseRateCritical(pulseRate)) {
+    displayWarning("Pulse Rate is out of range!");
     return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
+  } 
+  if (isSpo2Critical(spo2)) {
+    displayWarning("Oxygen Saturation out of range!");
     return 0;
   }
   return 1;
